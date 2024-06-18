f92d7377814fe0f84a008741605599fab8deb4c5
Hameed M <41132248+hameed-m@users.noreply.github.com>
2024-06-18 08:36:38 +0000

    redesign

--- src/views/Sections.vue
+++ src/views/Sections.vue
@@ -25,30 +25,30 @@ onMounted(() => {
       json.sort((a, b) => (a.stars > b.stars ? -1 : a.stars < b.stars ? 1 : 0))
     )
     .then((sorted) => (students.value = sorted))
-    .then(()=> students.value.map((value, index)=>value['order']=index+1));
+    .then(() =>
+      students.value.map((value, index) => (value["order"] = index + 1))
+    );
 });
 </script>
- 
+
 <template>
-  <v-app-bar
-    style="left: auto; min-width: 50%"
-    color="green"
-    scroll-behavior="collapse"
-  >
-    <h3 style="text-align: center; width: 100%">{{ section.name }}</h3>
+  <v-app-bar style="left: auto !important" scroll-behavior="collapse">
+    <h2 style="text-align: center; width: 100%">{{ section.name }}</h2>
   </v-app-bar>
   <v-container>
     <v-row>
       <v-col
-        v-for="(student) in students"
+        v-for="student in students"
         :key="student.id"
         cols="12"
         sm="12"
         md="6"
         lg="4"
       >
-        <v-card>
-          <v-card-title><h3>{{student.order}}</h3></v-card-title>
+        <v-card variant="flat">
+          <v-card-title
+            ><h3>{{ student.order }}</h3></v-card-title
+          >
           <v-card-text>
             <v-avatar v-if="student.picture" :src="student.picture" />
             <div class="student-info">
@@ -74,9 +74,13 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.v-app-bar {
+  min-width: 50%;
+  background-color: #f5f7d9;
+}
 .v-card-title {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .student-info {
@@ -100,14 +104,106 @@ onMounted(() => {
   font-size: 25px;
 }
 .v-card-text {
-  background: linear-gradient(to right, #f7cac9, #f5f7d9);
   border-radius: 4px; /* Add some rounded corners */
   padding: 1rem;
-
+}
+.v-row {
+  background: linear-gradient(to right, #f7cac9, #f5f7d9);
 }
 .v-card {
   display: flex;
   flex-direction: row;
-  border: 2px solid #ffeb3b; /* Example yellow border */
+  border: solid #ffffff;
+  border-radius: 0px;
+  border-width: 5px;
+  background-color: #f5f7d900;
+  height: 13vh;
+}
+.v-col-xxl,
+.v-col-xxl-auto,
+.v-col-xxl-12,
+.v-col-xxl-11,
+.v-col-xxl-10,
+.v-col-xxl-9,
+.v-col-xxl-8,
+.v-col-xxl-7,
+.v-col-xxl-6,
+.v-col-xxl-5,
+.v-col-xxl-4,
+.v-col-xxl-3,
+.v-col-xxl-2,
+.v-col-xxl-1,
+.v-col-xl,
+.v-col-xl-auto,
+.v-col-xl-12,
+.v-col-xl-11,
+.v-col-xl-10,
+.v-col-xl-9,
+.v-col-xl-8,
+.v-col-xl-7,
+.v-col-xl-6,
+.v-col-xl-5,
+.v-col-xl-4,
+.v-col-xl-3,
+.v-col-xl-2,
+.v-col-xl-1,
+.v-col-lg,
+.v-col-lg-auto,
+.v-col-lg-12,
+.v-col-lg-11,
+.v-col-lg-10,
+.v-col-lg-9,
+.v-col-lg-8,
+.v-col-lg-7,
+.v-col-lg-6,
+.v-col-lg-5,
+.v-col-lg-4,
+.v-col-lg-3,
+.v-col-lg-2,
+.v-col-lg-1,
+.v-col-md,
+.v-col-md-auto,
+.v-col-md-12,
+.v-col-md-11,
+.v-col-md-10,
+.v-col-md-9,
+.v-col-md-8,
+.v-col-md-7,
+.v-col-md-6,
+.v-col-md-5,
+.v-col-md-4,
+.v-col-md-3,
+.v-col-md-2,
+.v-col-md-1,
+.v-col-sm,
+.v-col-sm-auto,
+.v-col-sm-12,
+.v-col-sm-11,
+.v-col-sm-10,
+.v-col-sm-9,
+.v-col-sm-8,
+.v-col-sm-7,
+.v-col-sm-6,
+.v-col-sm-5,
+.v-col-sm-4,
+.v-col-sm-3,
+.v-col-sm-2,
+.v-col-sm-1,
+.v-col,
+.v-col-auto,
+.v-col-12,
+.v-col-11,
+.v-col-10,
+.v-col-9,
+.v-col-8,
+.v-col-7,
+.v-col-6,
+.v-col-5,
+.v-col-4,
+.v-col-3,
+.v-col-2,
+.v-col-1 {
+  width: 100%;
+  padding: 0px;
 }
 </style>
